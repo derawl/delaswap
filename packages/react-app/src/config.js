4ec1992de64063cb7c8156ce6e279e23043dff65
JesusKid <frawllings@gmail.com>
2024-01-26 09:17:35 +0100

    react:app changed

--- packages/react-app/src/config.js
+++ packages/react-app/src/config.js
@@ -5,6 +5,6 @@ export const ROUTER_ADDRESS = "0x37D674A6d93eDDC83866cbF89eD77aEbADA82D4C";
 export const DAPP_CONFIG = {
   readOnlyChainId: Goerli.chainId,
   readOnlyUrls: {
-    [Goerli.chainId]: "https://eth-goerli.g.alchemy.com/v2/GISCg6BXnxwQXYXLDoGRRznxtezF3U6_",
+    [Goerli.chainId]: "https://rpc.ankr.com/eth_goerli",
   },
 };
